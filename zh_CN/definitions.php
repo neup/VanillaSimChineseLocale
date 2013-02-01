7275dd59621966b69925915a88f0075c6dbca7c2
Feather Andelf <andelf@gmail.com>
2013-02-01 08:27:47 +0800

    initial release 1.2.1

---
+++ zh_CN/definitions.php
@@ -0,0 +1,29 @@
+<?php if (!defined('APPLICATION')) exit();
+/*
+Copyright 2010 NeuPioneer.
+This file is not part of Garden.
+Garden is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+Garden is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with Garden.  If not, see <http://www.gnu.org/licenses/>.
+NeuPioneer @ NEU of China
+*/
+
+$LocaleInfo['zh_CN'] = array(
+   'Locale' => 'zh-CN',
+   'Name' => 'Simplified Chinese Locale(简体中文)',
+   'Description' => 'Full Simplified Chinese Translation For Vanilla 2. 简体中文翻译, 来自东北大学先锋网技术部.',
+   'Version' => '1.2.1',
+   'Author' => "andelf",
+   'AuthorEmail' => 'andelf@gmail.com',
+   'AuthorUrl' => 'http://www.neupioneer.com',
+   'License' => 'GPLv3'
+);
+
+/*
+if (!function_exists('FormatPossessiveCustom')) {
+  function FormatPossessiveCustom($Word) {
+    return $Word.'的';
+  }
+}
+
+*/
